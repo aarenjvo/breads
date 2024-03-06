030c3ceccf064eaa21b5c60aa760b047874df874
Aaren Vo <aarenjvo@gmail.com>
2024-03-05 21:10:44 -0800

    added new/show.jsx and new routes

--- models/bread.js
+++ models/bread.js
@@ -2,21 +2,21 @@ module.exports = [
     {
         name: 'Rye',
         hasGluten: true,
-        image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?'
+        image: 'https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name: 'French',
         hasGluten: true,
-        image: ''
+        image: 'https://images.unsplash.com/photo-1559811814-e2c57b5e69df?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name: 'Gluten-Free',
         hasGluten: false,
-        image: ''
+        image: 'https://plus.unsplash.com/premium_photo-1700398889960-1c0237801a08?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name: 'Pumpernickel',
         hasGluten: true,
-        image: ''
+        image: 'https://images.unsplash.com/photo-1596662864674-60ade345dbef?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
 ]
