65b43f6ef3573947f16ffe367ca7474a905c1f08
Aaren Vo <aarenjvo@gmail.com>
2024-03-07 20:12:20 -0800

    added edit.jsx and delete/put methods

--- controllers/bread.js
+++ controllers/bread.js
@@ -17,7 +17,16 @@ router.get('/new', (req, res) => {
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.render('show', {
-        bread: Bread[index]
+        bread: Bread[index],
+        index
+    })
+})
+
+router.get('/:index/edit', (req, res) => {
+    const { index } = req.params
+    res.render('edit', {
+        bread: Bread[index],
+        index
     })
 })
 
@@ -30,6 +39,26 @@ router.post('/', (req, res) => {
         req.body.hasGluten = false
     }
     Bread.push(req.body)
+    res.redirect('/bread')
+})
+
+// UPDATE w/ PUT bread
+router.put('/:index', (req, res) => {
+    const { index } = req.params
+    if (!req.body.image) req.body.image = 'https://houseofnasheats.com/wp-content/uploads/2022/02/French-Bread-1.jpg'
+    if (req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[ index ] = req.body
+    res.redirect(`/bread/${ index }`)
+})
+
+// DELETE bread
+router.delete('/:index', (req, res) => {
+    const { index } = req.params
+    Bread.splice(index, 1)
     res.redirect('/bread')
 })
 
